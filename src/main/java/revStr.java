4d639f24f6456dc985ea44745547125c496b3684
arsenal <strigunovqa@gmail.com>
2018-01-30 21:39:55 -0800

    reverse string brush up :)

---
+++ src/main/java/revStr.java
@@ -0,0 +1,28 @@
+public class singleNum {
+
+    public static String reverseString(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            sb.append(s.charAt(i));
+
+        }
+
+        return sb.toString();
+
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(reverseString("baRashka"));
+
+
+    }
+
+}
+
+
+
