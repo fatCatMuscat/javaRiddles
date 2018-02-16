bb52d5156bd24ed0836a49ff17b1f8119fee5802
arsenal <strigunovqa@gmail.com>
2018-02-15 17:47:10 -0800

    added uniqueChars riddle

--- src/main/java/DataStructuresBook/r_1_2.java
+++ src/main/java/DataStructuresBook/r_1_2.java
@@ -6,15 +6,9 @@ public class r_1_2 {
         return i == n / m;
     }
 
-
-
     public static boolean isEven(int i) {
         return i % 2 == 0;
     }
-
-
-
-
 
     public static void main(String[] args) {
         System.out.println(isEven(30));
