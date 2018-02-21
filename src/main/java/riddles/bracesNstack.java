a515d1e1e688509d5a29e8afdb4236d34ed3afdd
arsenal <strigunovqa@gmail.com>
2018-02-20 18:54:39 -0800

    solution to braces

--- src/main/java/riddles/bracesNstack.java
+++ src/main/java/riddles/bracesNstack.java
@@ -1,4 +1,39 @@
 package riddles;
 
+import org.testng.annotations.Test;
+
+import java.util.Stack;
+
 public class bracesNstack {
+
+    @Test
+    public void isValidTest() {
+        String braces = "()((({})))";
+        System.out.println(isValid(braces));
+    }
+
+
+
+    private boolean isValid(String braces) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for (char ch: braces.toCharArray()){
+
+            switch (ch) {
+                case '(': stack.push(')');
+                break;
+                case '{': stack.push('}');
+                break;
+                case '[': stack.push(']');
+                break;
+                default:
+                    if (stack.isEmpty() || stack.pop() != ch) {
+                        return false;
+                    }
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+
 }
